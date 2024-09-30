811ca0298a8e6007583682a108805a60c4881842
sylide <71720482+s-ylide@users.noreply.github.com>
2024-10-01 01:05:19 +0900

    add modules

---
+++ rust-horn/src/library/syntax.rs
@@ -0,0 +1,50 @@
+#[derive(Debug, Clone)]
+pub struct Ident(String);
+
+#[derive(Debug, Clone)]
+pub struct Block {
+    // pub brace_token: Brace,
+    pub stmts: Vec<Stmt>,
+}
+
+#[derive(Debug, Clone)]
+pub enum Stmt {
+    Local(Local),
+    Expr(Expr),
+}
+
+#[derive(Debug, Clone)]
+pub struct Local {
+    pub pat: Pat,
+    pub init: Option<LocalInit>,
+}
+
+#[derive(Debug, Clone)]
+pub enum Pat {
+    Ident(Ident),
+    Tuple(PatTuple),
+}
+
+#[derive(Debug, Clone)]
+pub struct PatTuple {
+    // pub paren_token: Paren,
+    pub elems: Vec<Pat>,
+}
+
+#[derive(Debug, Clone)]
+pub struct LocalInit {
+    // pub eq_token: Eq,
+    pub expr: Box<Expr>,
+}
+
+#[derive(Debug, Clone)]
+pub enum Expr {
+    Assign(ExprAssign),
+}
+
+#[derive(Debug, Clone)]
+pub struct ExprAssign {
+    pub left: Box<Expr>,
+    // pub eq_token: Eq,
+    pub right: Box<Expr>,
+}
